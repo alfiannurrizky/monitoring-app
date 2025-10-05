30b0c79b470c1a7112733bd03270151eea3e2089
alfiannurrizky <alfiannrzky0@gmail.com>
2025-10-05 15:33:58 +0700

    fix vite configurations

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default defineConfig({
     server: {
         host: "0.0.0.0",
+        port: Number(process.env.VITE_PORT) || 5173,
+        strictPort: true,
+        cors: true,
+        origin: process.env.APP_URL,
         hmr: {
-            host: "localhost",
+            host: process.env.VITE_HOST || "localhost",
         },
     },
     plugins: [
